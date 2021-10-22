dc97b9d3c6e0d7590b3254c939615035d101c724
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2021-10-22 23:00:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
-
+import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Switch>
+          <Route path="/search">
+            {/* <SearchPage /> */}
+            <h1>This is search page </h1>
+          </Route>
+          
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      {/* <h1>Shivam Google Clone </h1> */}
+      {/* <Home/> */}
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Home.css'
+// import React from "react";
+import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps"
+import { Avatar } from '@material-ui/core';
+
+const Home = () => {
+    return (
+        <div className="home">
+            {/* <h1>This is home page</h1> */}
+            <div className="home_header">
+                <div className="home_header_left">
+                    <Link to="/about">About</Link>
+                    <Link to="/store">Store</Link>
+                </div>
+                <div className="home_header_right">
+                    <Link to="/gmail">Gmail</Link>
+                    <Link to="/images">Images</Link>
+                    <AppsIcon/>
+                    <Avatar/>
+                </div>
+            </div>
+            <div className="home_body">
+                <img src="https://1000logos.net/wp-content/uploads/2021/05/Google-logo.png" width="272" height="92" alt="" srcset="" />
+            </div>
+        </div>
+    )
+}
+
+export default Home
